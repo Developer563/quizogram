fa70f7d2f5119d10d6d7232eee64efadabed29b7
Developer563 <dev563896@protonmail.com>
2022-10-08 10:07:51 +0200

    pracuje nad odczytem z pliku

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ConsoleApp1.klasy;
 using System.Data;
+using System.IO;
 namespace ConsoleApp1
 {
     internal class Program
--- ConsoleApp1/ConsoleApp1/klasy/funkcje.cs
+++ ConsoleApp1/ConsoleApp1/klasy/funkcje.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ConsoleApp1.klasy;
+using System.IO;
 namespace ConsoleApp1.klasy
 {
     internal class funkcje
